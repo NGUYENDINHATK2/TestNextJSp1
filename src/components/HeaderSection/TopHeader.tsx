64580231a0d85f9a96bbae3ac135843dbf4d31c4
NGUYENDINHATK2 <dinhatnguyen81@gmail.com>
2023-02-21 20:21:04 +0700

    lol

--- src/components/HeaderSection/TopHeader.tsx
+++ src/components/HeaderSection/TopHeader.tsx
@@ -2,7 +2,23 @@
 const TopHeader = () => {
     return (
         <div className="bg-[#f6f4f4] px-64">
-                <h1>ok</h1>
+            <div className="w-full">
+                <ul className="flex w-full ">
+                    <li>
+                        <a href="">vesoonline.com</a>
+                    </li>
+                    <li>
+                        <a href="">doisotrung.com.vn</a>
+                    </li>
+                    <li>
+                        <a href="">sanpham.minhchinh.com</a>
+                    </li>
+                    <li className="flex flex-1  justify-end">
+                        Thay đổi thông tin vé dò 
+                        <a href="">Tại đây</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
