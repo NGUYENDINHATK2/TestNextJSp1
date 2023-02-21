9f715195994bf817d2a1a40fbf90d6078c76b83c
NGUYENDINHATK2 <dinhatnguyen81@gmail.com>
2023-02-21 20:26:10 +0700

    lol

--- src/components/HeaderSection/TopHeader.tsx
+++ src/components/HeaderSection/TopHeader.tsx
@@ -3,11 +3,11 @@ const TopHeader = () => {
     return (
         <div className="bg-[#f6f4f4] px-64">
             <div className="w-full">
-                <ul className="flex w-full ">
+                <ul className="flex w-full text-[13px] space-x-1 text-gray-500 ">
                     <li>
                         <a href="">vesoonline.com</a>
                     </li>
-                    <li>
+                    <li className="border-r-2 border-l-2 px-1">
                         <a href="">doisotrung.com.vn</a>
                     </li>
                     <li>
